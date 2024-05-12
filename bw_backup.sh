8ca4c07735af67300b4a2c01a723f1ff6cd149c7
adusachev <usachev.alexe@gmail.com>
2024-05-12 03:00:20 +0300

    rename files

--- bw_backup.sh
+++ bw_backup.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 
-
 GPG_EMAIL=your_gpg_email@gmail.com
 BW_EMAIL=your_bitwarden_email@gmail.com
 
-read -p "Bitwarden Master Password (hidden): " -s BW_PASS
-export BW_SESSION=$(bw login $BW_EMAIL $BW_PASS --raw)
-if [ -z "$BW_SESSION" ];
-then
-    exit 1
-fi
+spinner=( '|' '/' '-' '/' )
 
-EXPORT_FILE_PREFIX="bw_export_"
-TIMESTAMP=$(date "+%Y_%m_%d_%H%M%S")
-ENC_OUTPUT_FILE=$EXPORT_FILE_PREFIX$TIMESTAMP.enc
+spin() {
+    while [ 1 ]
+    do
+        for stick in ${spinner[@]}
+        do
+            echo -ne "\r$stick"
+            sleep 0.2
+        done
+    done
+}
 
-bw export --raw --session $BW_SESSION --format json | gpg -o $ENC_OUTPUT_FILE -e -a -r $GPG_EMAIL
 
-bw logout > /dev/null
-unset BW_SESSION
-unset BW_PASS
-unset BW_ACCOUNT
+backup() {
+    read -p "Bitwarden Master Password (hidden): " -s BW_PASS
 
-echo -e "\n\nSuccessful vault backup to file $(realpath $ENC_OUTPUT_FILE) \n"
+    echo -e "\nCreating encrypted backup..."
+    spin &
+    spin_pid=$!
+   
+    export BW_SESSION=$(bw login $BW_EMAIL $BW_PASS --raw)
+    if [ -z "$BW_SESSION" ];
+    then
+        kill $spin_pid
+        exit 1
+    fi
+
+    EXPORT_FILE_PREFIX="bw_export_"
+    TIMESTAMP=$(date "+%Y_%m_%d_%H%M%S")
+    ENC_OUTPUT_FILE=$EXPORT_FILE_PREFIX$TIMESTAMP.enc
+    bw export --raw --session $BW_SESSION --format json | gpg -o $ENC_OUTPUT_FILE -e -a -r $GPG_EMAIL
+    bw logout > /dev/null
+    unset BW_SESSION
+    unset BW_PASS
+    unset BW_ACCOUNT
+    echo -e "\n\nSuccessful vault backup to file $(realpath $ENC_OUTPUT_FILE) \n"
+
+    kill $spin_pid
+}
+
+
+backup
---
+++ bw_backup_simple.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+GPG_EMAIL=your_gpg_email@gmail.com
+BW_EMAIL=your_bitwarden_email@gmail.com
+
+read -p "Bitwarden Master Password (hidden): " -s BW_PASS
+export BW_SESSION=$(bw login $BW_EMAIL $BW_PASS --raw)
+if [ -z "$BW_SESSION" ];
+then
+    exit 1
+fi
+
+EXPORT_FILE_PREFIX="bw_export_"
+TIMESTAMP=$(date "+%Y_%m_%d_%H%M%S")
+ENC_OUTPUT_FILE=$EXPORT_FILE_PREFIX$TIMESTAMP.enc
+
+bw export --raw --session $BW_SESSION --format json | gpg -o $ENC_OUTPUT_FILE -e -a -r $GPG_EMAIL
+
+bw logout > /dev/null
+unset BW_SESSION
+unset BW_PASS
+unset BW_ACCOUNT
+
+echo -e "\n\nSuccessful vault backup to file $(realpath $ENC_OUTPUT_FILE) \n"
